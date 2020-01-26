16cab89cf85ca08c0017b7d0d874db0efe01045a
Valmir Correa <valmir.correa@ufrn.edu.br>
2020-01-26 09:51:07 -0300

    estrututa do projeto

---
+++ apiAluno/src/main/java/br/com/vcsjr/apiAluno/ApiAlunoApplication.java
@@ -0,0 +1,13 @@
+package br.com.vcsjr.apiAluno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAlunoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAlunoApplication.class, args);
+	}
+
+}
