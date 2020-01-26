dd3530cf0b7064b6c37ddc097eb953fd420193ef
Valmir Correa <valmir.correa@ufrn.edu.br>
2020-01-26 09:50:23 -0300

    projeto base

---
+++ apiAluno/src/main/java/br/com/vcsjr/apiAluno/controller/AlunoController.java
@@ -0,0 +1,11 @@
+package br.com.vcsjr.apiAluno.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 
+ */
+@RestController
+public class AlunoController {
+    
+}
